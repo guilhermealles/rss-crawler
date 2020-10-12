190ced13e6b952930c95c46ed2a23111838a32ab
Guilherme Alles <guilherme.r.alles@gmail.com>
2020-10-12 00:51:36 -0300

    Cleanup

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,6 @@ import { healthCheckApi } from "./infrastructure/api/healthCheckApi";
 
 const app = express();
 
-// app.get("/feed", async (_, res) => {
-//   const feed = await getFeed(
-//     new URL("https://lorem-rss.herokuapp.com/feed?unit=second&interval=30")
-//   );
-//   res.send(feed);
-// });
-
 app.use(healthCheckApi());
 app.use(feedApi());
 
